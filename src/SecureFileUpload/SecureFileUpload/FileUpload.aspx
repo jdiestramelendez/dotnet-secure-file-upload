db288f5bfcf596711dde00b84f3de1f13b26010d
Roberto Mardeni <romarden@microsoft.com>
2020-10-13 15:12:47 +0000

    Initial commit

---
+++ src/SecureFileUpload/SecureFileUpload/FileUpload.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FileUpload.aspx.cs" Inherits="SecureFileUpload.FileUpload" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <div class="container">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">File List</h3>
+            </div>
+            <div class="panel-body">
+                <div class="container-fluid">
+                    <asp:DataList ID="dlFiles" runat="server" DataKeyField="Value" OnItemCommand="dlFiles_ItemCommand">
+                        <ItemTemplate>
+                            <div class="row">
+                                <div class="col-md-6"><%# Eval("Value") %></div>
+                                <div class="col-md-6">
+                                    <asp:LinkButton ID="lbDeleteFile" runat="server" CommandName="DeleteFile" CommandArgument='<%# Eval("Value") %>' class="btn btn-danger btn-xs">
+                                <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                    </asp:LinkButton>
+                                </div>
+                            </div>
+                        </ItemTemplate>
+                    </asp:DataList>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">File Upload</h3>
+            </div>
+            <div class="panel-body">
+                <asp:Label ID="lblFile" runat="server" Text="CSV File:"></asp:Label>
+                <asp:FileUpload ID="fuCsvFile" runat="server" />
+                <br />
+                <asp:Button ID="btnSubmit" runat="server" Text="Submit" CssClass="btn btn-primary" OnClick="btnSubmit_Click" />
+            </div>
+        </div>
+
+        <div class="panel panel-danger" visible="false" runat="server" id="pnlError">
+            <div class="panel-heading">
+                <h3 class="panel-title">File Upload Error</h3>
+            </div>
+            <div class="panel-body">
+                <asp:Literal ID="litError" runat="server"></asp:Literal>
+            </div>
+        </div>
+
+        <div class="panel panel-default" visible="false" runat="server" id="pnlResult">
+            <div class="panel-heading">
+                <h3 class="panel-title">File Upload Results</h3>
+            </div>
+            <div class="panel-body">
+                <asp:Literal ID="litResults" runat="server"></asp:Literal>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ src/SecureFileUpload/SecureFileUpload/FileUpload.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace SecureFileUpload
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UpdateFileList();
+            ResetForm();
+        }
+
+        private void UpdateFileList()
+        {
+            string SaveLocation = Server.MapPath("App_Data");
+
+            var items = new List<ListItem>();
+
+            foreach (string filepath in System.IO.Directory.GetFiles(SaveLocation))
+            {
+                string filename = System.IO.Path.GetFileName(filepath);
+                items.Add(new ListItem { Text = filename, Value = filename });
+            }
+
+            dlFiles.DataSource = items;
+            dlFiles.DataBind();
+        }
+
+        protected void dlFiles_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if (e.CommandName == "DeleteFile")
+            {
+                string file = e.CommandArgument as string;
+                string SaveLocation = System.IO.Path.Combine(Server.MapPath("App_Data"), file);
+                System.IO.File.Delete(SaveLocation);
+                ShowResult($"Deleted {file}");
+                UpdateFileList();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (fuCsvFile.PostedFile != null)
+            {
+                if (fuCsvFile.PostedFile.ContentLength > 0)
+                {
+                    string fn = System.IO.Path.GetFileName(fuCsvFile.PostedFile.FileName);
+                    string SaveLocation = System.IO.Path.Combine(Server.MapPath("App_Data"), fn);
+                    try
+                    {
+                        fuCsvFile.PostedFile.SaveAs(SaveLocation);
+                        ShowResult("The file has been uploaded.");
+                        UpdateFileList();
+                    }
+                    catch (Exception ex)
+                    {
+                        ShowError(ex.Message);
+                    }
+                }
+                else
+                {
+                    ShowError("Empty file uploaded.");
+                }
+            }
+            else
+            {
+                ShowResult("Please select a file to upload.");
+            }
+        }
+
+        private void ResetForm()
+        {
+            pnlResult.Visible = false;
+            pnlError.Visible = false;
+        }
+
+        private void ShowResult(string resultMessage)
+        {
+            pnlResult.Visible = true;
+            pnlError.Visible = false;
+            litResults.Text = resultMessage;
+        }
+
+        private void ShowError(string errorMessage)
+        {
+            pnlResult.Visible = false;
+            pnlError.Visible = true;
+            litError.Text = errorMessage;
+        }
+    }
+}
