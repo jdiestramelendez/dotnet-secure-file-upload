169dc04c313b7fba12043931b883ed10e1fa0e6f
Roberto Mardeni <romarden@microsoft.com>
2020-10-16 17:16:41 +0000

    Added a file generator and 2 sample files of 1mb & 5mb

---
+++ src/SecureFileUpload/FileGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace FileGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string header = @"ID,Name,Description,Type";
+            string filename = "file.csv";
+            long numberOfLines = 100;
+
+            Console.WriteLine($"Enter filename ({filename}):");
+            var r = Console.ReadLine();
+            if (!string.IsNullOrEmpty(r))
+                filename = r;
+            Console.WriteLine($"Enter number of lines (default {numberOfLines}):");
+            r = Console.ReadLine();
+            if (!string.IsNullOrEmpty(r))
+                numberOfLines = int.Parse(r);
+
+            using (StreamWriter sw = File.AppendText(filename))
+            {
+                sw.WriteLine(header);
+                for (int i = 1; i <= numberOfLines; i++)
+                {
+                    string type = "None";
+                    if (i % 2 == 0) type = "New";
+                    if (i % 3 == 0) type = "Update";
+                    sw.WriteLine($"{i},Name of item {i},Description of item {i}, {type}");
+                }
+            }
+        }
+    }
+}
